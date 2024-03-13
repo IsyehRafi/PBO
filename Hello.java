5006a57881083e36d7872ac697cce03cb2dc4018
IsyehRafi <rafi7251@gmail.com>
2024-03-13 13:46:48 +0700

    coba 1

---
+++ Hello.java
@@ -0,0 +1,9 @@
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, nama saya Isyeh Rafi Maulana");
+        System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+        System.out.println("Saya sedang menulis program java");
+    }
+}
