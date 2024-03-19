09a6ebbec7c9c6af957bba64893f345f964e4705
IsyehRafi <112856402+IsyehRafi@users.noreply.github.com>
2024-03-19 23:24:16 +0700

    Add files via upload

---
+++ week2/inputkrs.java
@@ -0,0 +1,23 @@
+
+public class inputkrs {
+    public static void main (String[] args) {
+        //TODO Auto-generated method stub
+        krs krs1 = new krs();
+
+        krs1.setMataKuliah("Pemrograman Web", 4);
+        krs1.setMataKuliah("PBO", 2);
+        krs1.setMataKuliah("Dasar Pemrograman", 1);
+        krs1.setMataKuliah("Analisa dan Perancangan", 0);
+        krs1.setMataKuliah("Pemrograman Game", 3);
+
+
+        krs1.setSks(3, 0);
+        krs1.setSks(4, 1);
+        krs1.setSks(2, 2); 
+        krs1.setSks(3, 3);
+        krs1.setSks(2, 4);
+
+        krs1.infoKrs();
+    }
+    
+}
